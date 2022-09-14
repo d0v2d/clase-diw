ecf80929abfd83c675246ef1591c200a7ec03244
David Martín <davidmardecas@gmail.com>
2022-09-14 19:30:14 +0200

    commit inicial

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let n1 = "";
+let n2 = "";
+
+n1 = prompt("Introduce un número", n1);
+n2 = prompt("Introduce un número", n2);
+
+
+let suma = parseFloat(n1) + parseFloat(n2);
+
+console.log(suma);
+
+alert(suma);
+
+document.write("<h1>"+ suma +"</h1>");
